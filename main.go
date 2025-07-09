e83b5721577ea8b71793438dc41bd5973c182ba8
Hidoxvll <thend621@gmail.com>
2025-07-09 13:14:52 +0300

    main

--- main.go
+++ main.go
@@ -2,25 +2,43 @@ package main
 
 import "fmt"
 
-type Animal interface {
-	Speack() string
-}
-type Dog struct{}
+//
+//import "fmt"
+//
+//type Animal interface {
+//	Speack() string
+//}
+//type Dog struct {
+//}
+//
+//func (Dog) Speack() string {
+//	return "Woof!"
+//}
+//
+//type Cat struct {
+//}
+//
+//func (Cat) Speack() string {
+//	return "Nice to meet you!"
+//}
+//
+//func main() {
+//	var a Animal
+//
+//	a = Dog{}
+//	fmt.Printf("Dog voice: %s\n", a.Speack())
+//	a = Cat{}
+//	fmt.Printf("Cat voice: %s\n", a.Speack()
 
-func (Dog) Speack() string {
-	return "Woof!"
-}
-
-type Cat struct{}
-
-func (Cat) Speack() string {
-	return "Nice to meet you!"
+func chek(i interface{}) {
+	fmt.Printf("Тип: %T, Значение: %v\n", i, i)
 }
 
 func main() {
-	var a Animal
-	a = Dog{}
-	fmt.Printf("Dog voice: %s\n", a.Speack())
-	a = Cat{}
-	fmt.Printf("Cat voice: %s\n", a.Speack())
+	chek(123)
+	chek("Salom")
+	chek(12.21)
+	chek(true)
+	chek([]string{"Чёрный", "Белый", "Голубой"})
+
 }
